21be90a2c1370b8a4d618a412d1bb2c6c6760469
anna-sahradyan <a.sahradyan72@gmail.com>
2023-05-19 21:55:52 +0400

    create services component

--- figma/src/components/Home.jsx
+++ figma/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SliderComponent from "./SliderComponent";
 import Call from "./Call";
+import Service from "./Service";
 
 const Home = () => {
     return (
         <>
            <SliderComponent/>
             <Call/>
+            <Service/>
         </>
     );
 };
---
+++ figma/src/components/Service.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <>
+            <section className="services">
+                <div className="container">
+                    <div className="services__inner">
+        <div className="services__head">
+            <h4 className="services__title title">Сервис</h4>
+            <p className="services__desc">От идеи до незабываемого и
+                измеримого результата</p>
+            <p className="services__text">Интеграция безупречного производства, передовых технологий и тщательного измерения производительности</p>
+
+        </div>
+                        <div className="services__items">
+                            <div className="services__item">
+                                <div className="services__item-inner">
+                                    <div className="services__item-img"><img src="/img/icon1.svg" alt=""/></div>
+                                    <p className="services__item-title">СТРАТЕГИЯ</p>
+                                    <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                    <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+                                    
+                                </div>
+                            </div>
+                            <div className="services__item">
+                                <div className="services__item-inner">
+                                    <div className="services__item-img" ><img  src="/img/icon2.svg" alt=""/></div>
+                                    <p className="services__item-title">АНАЛИЗ</p>
+                                    <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                    <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                                </div>
+                            </div><div className="services__item">
+                            <div className="services__item-inner">
+                                <div className="services__item-img"><img src="/img/icon3.svg" alt=""/></div>
+                                <p className="services__item-title">СОЦ СЕТИ</p>
+                                <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                            </div>
+                        </div><div className="services__item">
+                            <div className="services__item-inner">
+                                <div className="services__item-img"><img src="/img/icon4.svg" alt=""/></div>
+                                <p className="services__item-title">РАЗВИТИЕ</p>
+                                <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                            </div>
+                        </div><div className="services__item">
+                            <div className="services__item-inner">
+                                <div className="services__item-img"><img src="/img/icon5.svg" alt=""/></div>
+                                <p className="services__item-title">КОНТЕНТ</p>
+                                <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                            </div>
+                        </div><div className="services__item">
+                            <div className="services__item-inner">
+                                <div className="services__item-img"><img src="/img/icon6.svg" alt=""/></div>
+                                <p className="services__item-title">ДИЗАЙН UX</p>
+                                <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                            </div>
+                        </div>
+                            <div className="services__item">
+                            <div className="services__item-inner">
+                                <div className="services__item-img"><img src="/img/icon7.svg" alt=""/></div>
+                                <p className="services__item-title">ТАРГЕТИРОВАННАЯ
+                                    РЕКЛАМА</p>
+                                <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                            </div>
+                        </div>
+                            <div className="services__item">
+                                <div className="services__item-inner">
+                                    <div className="services__item-img"><img src="/img/icon8.svg" alt=""/></div>
+                                    <p className="services__item-title">ПРОДУКТ</p>
+                                    <p className="services__item-text">Создадим уникальный и удобный дизайн..</p>
+                                    <a href="#" className="services__item-link">ПОДРОБНЕЕ</a>
+
+                                </div>
+                            </div>
+
+
+
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Service;
